d31088ff0599c2033220bd26737db95444fe1b3e
Anjali Khant <anjali@Anjalis-MacBook-Pro.local>
2021-05-13 14:08:14 -0400

    CSS Added for View

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,6 +2,7 @@ class TagsController < ApplicationController
     def index
         if params[:type].present?
             @places = Place.eager_load(:tag).where("tags.tag_0 like ? or tag_1 like ? or tag_2 like ? or tag_3 like ?" , "%#{params[:type]}%", "%#{params[:type]}%", "%#{params[:type]}%", "%#{params[:type]}%")
+            @tag_type = params[:type]
         else
             @places = Place.eager_load(:tag)
         end
